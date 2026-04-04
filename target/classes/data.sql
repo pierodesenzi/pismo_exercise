c76640eb6ff67f46b1689933a469a5875647c1eb
pierodesenzi <piero.desenzi@gmail.com>
2026-04-04 19:14:53 -0300

    removing target/ from repo and adding gitignore

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO operation_types (operation_type_id, description) VALUES (1, 'COMPRA A VISTA');
-INSERT INTO operation_types (operation_type_id, description) VALUES (2, 'COMPRA PARCELADA');
-INSERT INTO operation_types (operation_type_id, description) VALUES (3, 'SAQUE');
-INSERT INTO operation_types (operation_type_id, description) VALUES (4, 'PAGAMENTO');
